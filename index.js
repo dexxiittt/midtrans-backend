ff4005226c0a989e444c0e4e05add3df63485bc3
addsite <dexxiittt@gmail.com>
2026-04-05 18:05:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,4 +34,7 @@ app.post('/create-transaction', async (req, res) => {
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log('Server running'))
+
+app.listen(PORT, () => {
+  console.log('Server running on port', PORT)
+})

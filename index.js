3559ca695e8cd3253e2ed91c6d3de3b65b684484
addsite <dexxiittt@gmail.com>
2026-04-05 19:21:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const midtransClient = require('midtrans-client')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+
+console.log("KEY:", process.env.MIDTRANS_SERVER_KEY)
 
 let snap = new midtransClient.Snap({
   isProduction: false,
@@ -14,28 +18,25 @@ app.get('/', (req, res) => {
 })
 
 app.post('/create-transaction', async (req, res) => {
-
-  const { order_id, gross_amount } = req.body
-
-  let parameter = {
-    transaction_details: {
-      order_id: order_id,
-      gross_amount: gross_amount
-    }
-  }
-
   try {
-    const transaction = await snap.createTransaction(parameter)
-    res.json({ token: transaction.token })
-  } catch (err) {
-  console.error("MIDTRANS ERROR:", err)
-  res.status(500).json({ error: err.message })
-}
+    const { order_id, gross_amount } = req.body
 
+    let parameter = {
+      transaction_details: {
+        order_id,
+        gross_amount
+      }
+    }
+
+    const transaction = await snap.createTransaction(parameter)
+
+    res.json({ token: transaction.token })
+
+  } catch (err) {
+    console.error("MIDTRANS ERROR:", err)
+    res.status(500).json({ error: err.message })
+  }
 })
 
 const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-  console.log('Server running on port', PORT)
-})
+app.listen(PORT, () => console.log('Server running'))

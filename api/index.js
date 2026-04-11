3cc65c93ab1b25e4ea02ed765e74e6a704223fc3
addsite <dexxiittt@gmail.com>
2026-04-11 15:45:35 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,8 @@
-export default function handler(req, res) {
-  if (req.method === "GET") {
-    return res.status(200).json({
-      message: "Backend jalan di Vercel 🚀"
-    });
-  }
+export default (req, res) => {
+  res.status(200).json({
+    message: "Backend jalan di Vercel 🚀"
+  });
+};
 
   return res.status(405).json({
     message: "Method not allowed"

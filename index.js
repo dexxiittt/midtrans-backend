fdf2b60df0c67a43b90db674023b052890f3f424
addsite <dexxiittt@gmail.com>
2026-04-11 11:20:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
+
 app.use(express.json())
 app.use(cors())
+
+console.log("PORT:", process.env.PORT)
 
 app.get('/', (req, res) => {
   res.send('Backend jalan 🚀')
 })
 
-const PORT = process.env.PORT || 3000
+app.get('/health', (req, res) => {
+  res.send('OK')
+})
+
+const PORT = process.env.PORT
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log('Server running on port', PORT)
 })
